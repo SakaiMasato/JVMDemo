cdd918004f7be0b3b48c115a349b786a7560350c
exiobao <bob.bao@ericsson.com>
2020-10-20 18:28:23 +0800

    Initial commit

---
+++ chapter01/src/com/bob/java/StackStruTest.java
@@ -0,0 +1,11 @@
+package com.bob.java;
+
+//javap -v StackStruTest.class 反编译见stack.png
+public class StackStruTest {
+    public static void main(String[] args){
+        int i = 2;
+        int j = 3;
+        int k = i + j;
+
+    }
+}
